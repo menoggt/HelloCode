48470982eaa6bcd266bab99b46b6670a6fbb23a2
meno <glebguy8@gmail.com>
2023-09-19 14:01:23 +0500

    sos

---
+++ Example005_HelloZdarova/Program.cs
@@ -0,0 +1,12 @@
+﻿console.WriteLine("Введите имя пользователя");
+string username = console.Readline();
+
+if (username == "Глебастер");
+{
+    Console.Writeline("Здарова Глебас");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
