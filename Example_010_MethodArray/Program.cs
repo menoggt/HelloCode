d2f986e093788c309cab4b446927d3ab7050ed07
meno <glebguy8@gmail.com>
2023-09-24 12:22:11 +0500

    010

---
+++ Example_010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] mas = {15,66,267,76,40,75,66,57,38,92,10};
+
+int n = mas.Length;
+int find = 66;
+
+int index = 0;
+
+while (index < n)
+{
+    if (mas[index] == find)
+    {
+        Console.WriteLine("Индекс числа: " + index);
+        break;
+    }
+    index += 1;
+}
