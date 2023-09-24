8eee5a1fb2318b3c850eb51b12b6d9cdcfb0f54f
meno <glebguy8@gmail.com>
2023-09-24 13:12:55 +0500

    011

---
+++ Example_011_ArrayLibrary/Program.cs
@@ -0,0 +1,57 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index += 1;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position += 1;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index += 1;
+    }
+    return position;
+}
+
+int[] mas = new int[10];
+
+FillArray(mas);
+mas[3] = 4;
+mas[5] = 4;
+PrintArray(mas);
+Console.WriteLine();
+
+int pos = IndexOf(mas, 4);
+if (pos == -1)
+{
+    Console.WriteLine("Не найдено");
+}
+else
+{
+    Console.WriteLine(pos);
+}
